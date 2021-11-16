0002da9cefd3aca6b550d6846beea90f3dab4ae9
sosulka7 <sosulka242@gmail.com>
2021-11-16 19:32:02 +0300

    rename controller

--- src/main/java/com/koshelev/spring/web/controllers/ProductController.java
+++ src/main/java/com/koshelev/spring/web/controllers/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
-public class MainController {
+public class ProductController {
     private ProductRepository productRepository;
 
     @Autowired
