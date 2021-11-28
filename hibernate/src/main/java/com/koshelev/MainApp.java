043e3668755d650bf1857cdba8f2ee24a4ae0f95
sosulka7 <sosulka242@gmail.com>
2021-11-28 17:03:54 +0300

    lesson 6

--- hibernate/src/main/java/com/koshelev/MainApp.java
+++ hibernate/src/main/java/com/koshelev/MainApp.java
@@ -2,22 +2,6 @@ package com.koshelev;
 
 public class MainApp {
     public static void main(String[] args) {
-        SessionFactoryUtils sessionFactory = new SessionFactoryUtils();
-        try {
-            sessionFactory.init();
-            ProductDao productDao = new ProductDaoImpl(sessionFactory);
-            //для теста update
-            Product product = productDao.findById(3L);
-            product.setCost(456.0);
-            //для теста save
-//            Product product = new Product("sauce", 56.23);
-            System.out.println(productDao.saveOrUpdate(product).getCost());
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            sessionFactory.shutdown();
-        }
     }
 
 
