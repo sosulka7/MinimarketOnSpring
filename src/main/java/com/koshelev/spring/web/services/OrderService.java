de7c886f9a897f3257aee1eaff61001a4ca31eb4
sosulka7 <sosulka242@gmail.com>
2022-01-12 18:23:30 +0300

    сохранение заказа в бд проходит корректно

--- src/main/java/com/koshelev/spring/web/services/OrderService.java
+++ src/main/java/com/koshelev/spring/web/services/OrderService.java
@@ -1,6 +1,7 @@
 package com.koshelev.spring.web.services;
 
 import com.koshelev.spring.web.dto.OrderDetailsDto;
+import com.koshelev.spring.web.dto.OrderItemDto;
 import com.koshelev.spring.web.entities.Order;
 import com.koshelev.spring.web.entities.OrderItem;
 import com.koshelev.spring.web.entities.User;
@@ -18,12 +19,22 @@ public class OrderService {
     private final UserService userService;
     private final CartService cartService;
     private final OrderItemService orderItemService;
+    private final ProductService productService;
 
     @Transactional
     public void createOrder(OrderDetailsDto odd, String username){
         User user = userService.findByUsername(username).orElseThrow(() -> new ResourceNotFoundException(String.format("Пользователь с никнеймом %s не найден.", username)));
         Order order = new Order(user, cartService.getCurrentCart().getTotalPrice(), odd.getAddress(), odd.getPhoneNumber());
         orderRepository.save(order);
-        orderItemService.saveOrderItems(cartService.getCurrentCart().getItems(), order);
+
+        for (OrderItemDto oid : cartService.getCurrentCart().getItems()) {
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrderId(order);
+            orderItem.setProductId(productService.getProductById(oid.getProductId()).orElseThrow(()-> new ResourceNotFoundException("Продукт не найден. ID: " + oid.getProductId())));
+            orderItem.setPrice(oid.getPrice());
+            orderItem.setQuantity(oid.getQuantity());
+            orderItem.setPricePerProduct(oid.getPricePerProduct());
+            orderItemService.save(orderItem);
+        }
     }
 }
